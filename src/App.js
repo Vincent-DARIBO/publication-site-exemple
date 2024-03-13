890cc3be38677be3284dac2760b05bef68062028
Vincent <daribovincent@gmail.com>
2024-03-13 13:30:30 +0100

    fix: unused variables

--- src/App.js
+++ src/App.js
@@ -2,10 +2,33 @@ import dog from "./assets/dog.jpg";
 import "./App.css";
 import SearchBar from "./components/SearchBar";
 import TodoList from "./components/TodoList";
+import { Profiler } from "react";
 function App() {
+  console.log("render app");
   return (
     <div className="App">
-      <TodoList />
+      <Profiler
+        id="App"
+        onRender={(
+          id,
+          phase,
+          actualDuration,
+          baseDuration,
+          startTime,
+          commitTime
+        ) => {
+          console.log({
+            id,
+            phase,
+            actualDuration,
+            baseDuration,
+            startTime,
+            commitTime,
+          });
+        }}
+      >
+        <TodoList />
+      </Profiler>
     </div>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Profiler, useState } from "react";
 
 export default function TodoList() {
   const [searchValue, setSearchValue] = useState("");
@@ -29,32 +29,74 @@ export default function TodoList() {
   }
 
   return (
-    <div>
+    <Profiler
+      id="List container"
+      onRender={(
+        id,
+        phase,
+        actualDuration,
+        baseDuration,
+        startTime,
+        commitTime
+      ) => {
+        console.log({
+          id,
+          phase,
+          actualDuration,
+          baseDuration,
+          startTime,
+          commitTime,
+        });
+      }}
+    >
       <div>
-        <input
-          type="text"
-          value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
-        />
-        <button onClick={() => addItem(searchValue)}>ajouter</button>
+        <div>
+          <input
+            type="text"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+          <button onClick={() => addItem(searchValue)}>ajouter</button>
+        </div>
+        <Profiler
+          id="List"
+          onRender={(
+            id,
+            phase,
+            actualDuration,
+            baseDuration,
+            startTime,
+            commitTime
+          ) => {
+            console.log({
+              id,
+              phase,
+              actualDuration,
+              baseDuration,
+              startTime,
+              commitTime,
+            });
+          }}
+        >
+          <ul>
+            {list.map((item, index) => (
+              <li key={index}>
+                <button onClick={() => markAsCompleted(item)}>cocher</button>
+                <text
+                  style={{
+                    textDecorationLine: completedItems.includes(item)
+                      ? "line-through"
+                      : "none",
+                  }}
+                >
+                  {item}
+                </text>
+                <button onClick={() => removeItem(item)}>supprimer</button>
+              </li>
+            ))}
+          </ul>
+        </Profiler>
       </div>
-      <ul>
-        {list.map((item, index) => (
-          <li key={index}>
-            <button onClick={() => markAsCompleted(item)}>cocher</button>
-            <text
-              style={{
-                textDecorationLine: completedItems.includes(item)
-                  ? "line-through"
-                  : "none",
-              }}
-            >
-              {item}
-            </text>
-            <button onClick={() => removeItem(item)}>supprimer</button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    </Profiler>
   );
 }
